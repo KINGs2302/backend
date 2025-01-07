be2c3f49f574c8cc1d98b28d2abe6ea5da6d6b31
KINGs2302 <jainishrupala@gmail.com>
2025-01-07 11:27:58 +0530

    first commit

---
+++ datatype.js
@@ -0,0 +1,13 @@
+// # primitive
+
+//  7 types : String, number, boolean, null, undefine, bigint, symbol
+
+// refrence (non primitive)
+
+// arrys, objects, functions
+
+const score = 3312345678122n
+console.log(typeof s);
+
+
+
---
+++ first.js
@@ -0,0 +1,15 @@
+var nameId = "Jainish"
+//console.log("hii",nameId)
+
+// console.log();
+
+let some;
+
+let s = String(some)
+// console.log(typeof s);
+// console.log(s);
+
+console.log("2"===2);
+
+
+
